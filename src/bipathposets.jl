0f86455ec3474a7780cc035a05fca7c57a8880d5
ShunsukeTada1357 <123849098+ShunsukeTada1357@users.noreply.github.com>
2024-04-24 18:07:28 +0900

    Update bipathposets.jl
    code is rewrited

--- src/bipathposets.jl
+++ src/bipathposets.jl
@@ -1,13 +1,10 @@
-module bipathposets
-
-
+module Bipathposets
+include("IsFSC.jl")
 include("FSCvector.jl")
-#include("q.jl")
 include("FSC_Contraction_Persistence.jl")
 include("FSCtoAtypeMethod.jl")
 include("BipathMatMethod.jl")
-#include("clique_Aoki.jl")
+include("clique.jl")
 include("BipathPD.jl")
-
-end # module 
+end # module Bipathposets
 
