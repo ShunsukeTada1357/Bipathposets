3daaba0ed688de1710e20124105eacea730e849d
ShunsukeTada1357 <123849098+ShunsukeTada1357@users.noreply.github.com>
2024-04-25 20:42:46 +0900

    Update BipathMatMethod.jl

--- src/BipathMatMethod.jl
+++ src/BipathMatMethod.jl
@@ -1,11 +1,11 @@
-#This file is for the function bipathpersistence(FSCa,FSCb).
+#This file is for the function bipathpersistence(FSCa,FSCb), where the input FSCa and FSCb are FSC.
 using AbstractAlgebra
-export bipathpersistence
 R= AbstractAlgebra.GF(2)
-function separateintervals(pairs,k)#k is FSC[2], #This code separate intervals into four types.  
+
+#This code separate intervals into four types.
+function separateintervals(pairs,k)#k is FSC[2] 
     basis = collect(keys(pairs))
     intervals =[pairs[b] for b in basis ]
-    n = length(intervals)
     leftintervals = []
     rightintervals = []
     center = []
@@ -14,7 +14,7 @@ function separateintervals(pairs,k)#k is FSC[2], #This code separate intervals i
     othersbasis = []
     leftbasis = []
     rightbasis = []
-    for i in 1:n
+    for i in 1:length(intervals)
         if intervals[i] ==[1,k] # [1,"∞"]
             push!(center,intervals[i]) 
             push!(centerbasis, basis[i])
@@ -32,73 +32,76 @@ function separateintervals(pairs,k)#k is FSC[2], #This code separate intervals i
     return [leftintervals,leftbasis], [center,centerbasis], [rightintervals,rightbasis], [others,othersbasis] 
 end
 
-function get_basis_intervals_left(FSC,left_int_basis)   #The input "left_int_basis" is the result of the function separateintervals(-,-)[1].
+# the input "left_int_basis" is the result of the function separateintervals(-,-)[1].
+function get_basis_intervals_left(FSC,left_int_basis)   
     leftB = [vectorizationofSC(FSC,i) for i in left_int_basis[2]]
-    if length(leftB) == 0
-        return  matrix_space(R,length(FSC[1]),length(leftB))()#R=GF(2). Call the result Lsapce. 
-    else
-        return hcat(leftB...)
-    end
+    Lspace=matrix_space(R,length(FSC[1]),length(leftB))()#R=GF(2)
+    for i in 1:length(leftB)
+        Lspace[:,i]= leftB[i]
+    end 
+    return Lspace
 end
 
-function get_basis_intervals_right(FSC, right_int_basis)#The input "right_int_basis" is the result of the function separateintervals(-,-)[3].
+# the input "right_int_basis" is the result of the function separateintervals(-,-)[3].
+function get_basis_intervals_right(FSC, right_int_basis)# l=length(FSC[1])   
     rightB = [vectorizationofSC(FSC,i) for i in right_int_basis[2]]
-    if length(rightB) == 0
-        return  matrix_space(R,length(FSC[1]),length(rightB))()#R=GF(2), call the result Lsapce 
-    else
-        return hcat(rightB...)
-    end
+    Rspace=matrix_space(R,length(FSC[1]),length(rightB))()#R=GF(2)
+    for i in 1:length(rightB)
+        Rspace[:,i]= rightB[i]
+    end 
+    return Rspace
 end
 
 function get_basis_intervals_left(FSC,left_int_basis,imagebasis_left) 
+    l = length(FSC[1])
     leftB = [vectorizationofSC(FSC,i) for i in left_int_basis[2]]
-    if length(leftB) == 0
-         Lspace = matrix_space(R,length(FSC[1]), length(leftB))()#R=GF(2)
-    else
-        Lspace = hcat(leftB...)
+    Lspace = matrix_space(R,l,length(leftB))()#R=GF(2)
+    for i in 1:length(leftB)
+        Lspace[:,i] = leftB[i]
     end 
+
     leftBB = [vectorizationofSC(FSC,i) for i in imagebasis_left]
-    if length(leftBB) == 0
-        LLspace = matrix_space(R,length(FSC[1]), length(leftBB))()#R=GF(2)
-    else
-        LLspace = hcat(leftBB...)
-    end
-    return [Lspace LLspace]
+    LLspace = matrix_space(R,l,length(leftBB) )()#R=GF(2)
+    for i in 1:length(leftBB) 
+        LLspace[:,i] = leftBB[i]
+    end 
+    return  [Lspace LLspace]
 end
 
 function get_basis_intervals_right(FSC,right_int_basis,imagebasis,centerwithB) 
+    l = length(FSC[1])
     rightB = [vectorizationofSC(FSC,i) for i in right_int_basis[2]]
-    if length(rightB) == 0
-        Rspace=matrix_space(R,length(FSC[1]),length(rightB))()#R=GF(2)
-    else 
-        Rspace = hcat(rightB...)
-    end
+    cols1 = length(rightB)
+    Rspace=matrix_space(R,l,cols1)()#R=GF(2)
+    for i in 1:cols1
+        Rspace[:,i]= rightB[i]
+    end 
 
     rrightB=[vectorizationofSC(FSC,i[1]) for i in imagebasis]
-    if length(rrightB) == 0
-        RRspace=matrix_space(R,length(FSC[1]),length(rrightB) )()#R=GF(2)
-    else
-        RRspace= hcat(rrightB...)
-    end
-
+    cols2 = length(rrightB) 
+    RRspace=matrix_space(R,l,cols2)()#R=GF(2)
+    for i in 1:cols2
+        RRspace[:,i]= rrightB[i]
+    end 
+    
     rrrightB=[vectorizationofSC(FSC,i) for i in centerwithB[2]]
-    if length(rrrightB)  == 0
-        aspace=matrix_space(R,l,length(rrrightB) )()#R=GF(2)
-    else
-        aspace = hcat(rrrightB...)
+    cols3 = length(rrrightB) 
+    aspace=matrix_space(R,l,cols3)()#R=GF(2)
+    for i in 1:cols3
+        aspace[:,i]= rrrightB[i]
     end 
     return [Rspace RRspace aspace]
 end
 
 function get_two_repmat(LspaceUp,RspaceUp,LspaceDown,RspaceDown)
     m = size(LspaceUp)[2]
-    leftrepmat =solve(LspaceDown,LspaceUp)[1:m,1:m]
+    leftrepmat =solve(LspaceDown,LspaceUp,side = :right)[1:m,1:m]
     n = size(RspaceUp)[2]
-    rightrepmat =solve(RspaceDown,RspaceUp)[1:n,1:n]
+    rightrepmat =solve(RspaceDown,RspaceUp,side = :right)[1:n,1:n]
     return leftrepmat, rightrepmat
 end
-
-function connect_updown(upintervalswithB,downintervalswithB,matrix) #matrix method!!
+#matrixmethod!!
+function connect_updown(upintervalswithB,downintervalswithB,matrix)
     conn = []
     n = length(upintervalswithB[1])# number of intervals
     redmat = reducematfromLtoR(matrix)
@@ -111,7 +114,7 @@ end
 
 function _bipathpersistence(FSCa,FSCb)
     pairsa = baseswithintervals(FSCa)[1]
-    pairsb, imb_left, imb = baseswithintervals(FSCb)
+    pairsb, imb_left , imb = baseswithintervals(FSCb)
     dima=Set([length(k[1])-1 for k in keys(pairsa)]) #k=[[4, 5], [5, 6], [4, 6]] can be a basis of 1-dimensional homology
     dimb=Set([length(k[1])-1 for k in keys(pairsb)]) 
     dims = sort(collect(union(dima,dimb)))
@@ -124,23 +127,21 @@ function _bipathpersistence(FSCa,FSCb)
     RspaceUp = get_basis_intervals_right(FSCa,a[3])
     LsapceUp = get_basis_intervals_left(FSCa,a[1])  
     mats= get_two_repmat(LsapceUp,RspaceUp,LspaceDown,RspaceDown)
-    Lrepmat = mats[1]
-    Rrepmat = mats[2]
-    intL= connect_updown(a[1],b[1],Lrepmat)
-    intR = connect_updown(a[3],b[3],Rrepmat)
+    intL= connect_updown(a[1],b[1],mats[1])
+    intR = connect_updown(a[3],b[3],mats[2])
     return intL,intR,a[4],a[2],b[4],dims
 end
 
 function bipathpersistence(FSCa,FSCb)
     X = _bipathpersistence(FSCa,FSCb)
+    dims =X[6]
     i_thhomology = Dict()
-    for i in X[6]
+    for i in dims
        intL = [[int[1][1], int[2][1]] for int in X[1] if length(int[1][2][1])== i+1]
        intR = [[int[1][1], int[2][1]] for int in X[2] if length(int[1][2][1])== i+1]
        up= [X[3][1][a] for a in 1:length(X[3][1]) if length(X[3][2][a][1])==i+1 ]
-       center= [X[4][1][a] for a in 1:length(X[4][1]) if length(X[4][2][a][1])==i+1 ]
-       down = [X[5][1][a] for a in 1:length(X[5][1]) if length(X[5][2][a][1])==i+1]
-
+       center=[X[4][1][x] for x in 1:length(X[4][1]) if length(X[4][2][x][1])==i+1 ]
+       down =[ X[5][1][x] for x in 1:length(X[5][1]) if length(X[5][2][x][1])==i+1]
        i_thhomology[i] =  [intL, intR, up,center,down]
        if iszero(i_thhomology[i])
           println( "¬  ∃ ",i,"_th homology" )
@@ -152,7 +153,7 @@ function bipathpersistence(FSCa,FSCb)
        println("intervals up: ",up)
        println("intervals down: ",down)
        println("................")
-       end
+       end       
     end
     return i_thhomology, FSCa[2] ,FSCb[2]
 end
