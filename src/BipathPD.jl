8b5466d3b1260c272bc4e1a194ad9be54c97b8f6
ShunsukeTada1357 <123849098+ShunsukeTada1357@users.noreply.github.com>
2024-04-25 20:58:48 +0900

    Update BipathPD.jl

--- src/BipathPD.jl
+++ src/BipathPD.jl
@@ -1,5 +1,5 @@
 ##This file is for a function plotintlist(intlist,a,b)
-## that visualize bipath intervals. Input: intervals (represented by dimension vector), a,b: integers for rotating PD.    
+##That visualize bipath intervals. Input: intervals (represented by dimension vector), a,b: integers for rotating PD.    
 
 using Plots
 using LaTeXStrings
@@ -46,20 +46,6 @@ function persistenceDown(I)
     return I[2]-I[1]
 end
 
-
-function shaffle(list,a)##change name!! want good name
-    r = length(list)
-    a = a%(r)
-    if a == 0
-        return list
-    else
-        want=list[a+1:r]
-        for i in 1:a
-            push!(want,list[i])
-        end
-        return want
-    end
-end
 #(3) intervals to cartesian coordinate
 
 function intervaltocartesiancoordinateL(I,n,m)
@@ -98,10 +84,7 @@ function intervalstocartesiancoordinate(intL,intR,up,down,n,m)
     for I in down
         push!(intlist,intervaltocartesiancoordinateDown(I,n,m))
     end
-
-    #center = others[2]
     return intlist#, center
-
 end
 
 function intlisttointlistwithmultiplicity(intlist)#[I,I,J,J,J]=> [[I,2],[J,3]]
@@ -114,11 +97,8 @@ function intlisttointlistwithmultiplicity(intlist)#[I,I,J,J,J]=> [[I,2],[J,3]]
     return want
 end
 
-function intervalstocartesiancoordinatewithmulti(intlist,n,m)
+function intervalstocartesiancoordinatewithmulti(intlist)
     newintlistwith = intlisttointlistwithmultiplicity(intlist)
-    #n = length(Intlist[1][1])-2
-    #m = length(Intlist[1][2])-2
-    #R =n+m+2
     xlist = []
     ylist = []
     multi = []
@@ -127,68 +107,9 @@ function intervalstocartesiancoordinatewithmulti(intlist,n,m)
         push!(ylist, I[1][2])
         push!(multi, I[2])
     end
-    return xlist,ylist,multi,n,m
+    return xlist,ylist,multi
 end
-
-
-
 #(5) color of points means the number of intervals C_{n,m}
-function plotintlist(intL,intR,up,down,a,b,n,m)#####a=0,b=0
-    intlist = intervalstocartesiancoordinate(intL,intR,up,down,n,m)
-    plot()
-    XX = intervalstocartesiancoordinatewithmulti(intlist,n,m)
-    #a,b = 0,0
-    maxim = maximum(XX[3])
-    col =  cgrad(:lajolla50,rev =true )
-    num = length(col)
-    d = div(num,maxim)
-    n = XX[4]
-    m = XX[5]
-    R = n + m +2
-
-#color area where points are not plotted
-    #plot!([0, n+1], [0, n+1], fillrange=0, fillalpha=0.4, fillcolor=:lightblue,linewidth=0, msw=:0,label=false)
-    #plot!([5,R],[n+1,R],fillrange = [n+1,n+1],fillalpha = 0.4,fillcolor =:lightblue, msw=:0,linewidth=0, label=false)
-    
-for i in 1:length(XX[1])
-    #sizeofpoint= 3 + I[2]/all
-    plot!([(XX[1][i]+a)%R], [(XX[2][i]+b)%R], 
-    proj=:identity, ms=6,msw=:0,
-    m=:o, c =col[XX[3][i]*d], lt=:scatter,
-    markerstrokealpha=1, legend = false,
-    xlims=(-1,R), ylims=(-1,R),  
-    fa=0.4, xticks=[], yticks=[],
-    grid=true, left_margin = 10Plots.mm, 
-    bottom_margin = 10Plots.mm, 
-    aspect_ratio=:equal, label="")
-end
-
-vline!([(n+1+a)%R], linestyle=:dash, color=:red, label="1-hat ="*string(XX[4]+1))
-hline!([(n+1+b)%R], linestyle=:dash, color=:red, label="1-hat ="*string(XX[5]+1))
-vline!([a%R], linestyle=:dash, color=:blue, label="0-hat ="*string(XX[4]+1))
-hline!([b%R], linestyle=:dash, color=:blue, label="0-hat ="*string(XX[5]+1))
-# draw labels
-
-hat = L"\widehat{0}"
-lll=[hat]
-for i in 1:n 
-    push!(lll,string(i)) 
-end
-hat = L"\widehat{1}"
-
-push!(lll,hat)
-for i in 1:m 
-    push!(lll,string(m-i+1)*"'") 
-end
-uu = -1.5*ones(R)
-uuuu=shaffle(lll,(R-a)%R)
-annotate!(0:R-1, uu, text.(uuuu,18,"Computer Modern"))
-uu = -1.5*ones(R)
-uuuu=shaffle(lll,(R-b)%R)
-annotate!(uu,0:R-1, text.(uuuu,18,"Computer Modern"))
-current()
-end
-
 function colorfunc(col,r::Int)
     n = length(col)
     if r <= 1
@@ -200,82 +121,23 @@ function colorfunc(col,r::Int)
     end
 end
 
-function upper_than_diag(x)
-    if x[1]<= x[2]
-      return true
+function colorfunc2(col,max::Int,mult::Int)
+    n = length(col)
+    d = Int64(floor(n/max))
+    if d <= 1
+        return 1
+    elseif max == 1
+        return 1
     else
-      return false
+        return min(1+(d)*(mult-1),n)
     end
-  end
-
-function plotintlist(FSCa,FSCb,a,b,i_th)#####a=0,b=0
-    X =bipathpersistence(FSCa,FSCb,i_th)[i_th+1]
-    n = FSCa[2]-2
-    m = FSCb[2]-2
-    intL = X[1]
-    intR=X[2]
-    up = X[3]
-    down=X[5]
-    intlist = intervalstocartesiancoordinate(intL,intR,up,down,n,m)
-    plot()
-    XX = intervalstocartesiancoordinatewithmulti(intlist,n,m)
-    #a,b = 0,0
-    maxim = maximum(XX[3])
-    col =  cgrad(:lajolla50,rev =true )
-    num = length(col)
-    d = div(num,maxim)
-    n = XX[4]
-    m = XX[5]
-    RR = n + m +2
-
-#color area where points are not plotted
-    #plot!([0, n+1], [0, n+1], fillrange=0, fillalpha=0.4, fillcolor=:lightblue,linewidth=0, msw=:0,label=false)
-    #plot!([5,R],[n+1,R],fillrange = [n+1,n+1],fillalpha = 0.4,fillcolor =:lightblue, msw=:0,linewidth=0, label=false)
-    
-for i in 1:length(XX[1])
-    #sizeofpoint= 3 + I[2]/all
-    plot!([(XX[1][i]+a)%RR], [(XX[2][i]+b)%RR], 
-    proj=:identity, ms=6,msw=:0,
-    m=:o#,c =col[min(length(col),XX[3][i]*d)]
-    ,
-    c = col[colorfunc(col,XX[3][i]*d)],
-    lt=:scatter,
-    markerstrokealpha=1, legend = false,
-    xlims=(-1,RR), ylims=(-1,RR),  
-    fa=0.4, xticks=[], yticks=[],
-    grid=true, left_margin = 10Plots.mm, 
-    bottom_margin = 10Plots.mm, 
-    aspect_ratio=:equal, label="")
 end
 
-vline!([(n+1+a)%RR], linestyle=:dash, color=:red, label="1-hat ="*string(XX[4]+1))
-hline!([(n+1+b)%RR], linestyle=:dash, color=:red, label="1-hat ="*string(XX[5]+1))
-vline!([a%RR], linestyle=:dash, color=:blue, label="0-hat ="*string(XX[4]+1))
-hline!([b%RR], linestyle=:dash, color=:blue, label="0-hat ="*string(XX[5]+1))
-# draw labels
-
-hat = L"\widehat{0}"
-lll=[hat]
-for i in 1:n 
-    push!(lll,string(i)) 
-end
-hat = L"\widehat{1}"
-
-push!(lll,hat)
-for i in 1:m 
-    push!(lll,string(m-i+1)*"'") 
-end
-uu = -1.5*ones(RR)
-uuuu=shaffle(lll,(RR-a)%RR)
-annotate!(0:RR-1, uu, text.(uuuu,18,"Computer Modern"))
-uu = -1.5*ones(RR)
-uuuu=shaffle(lll,(RR-b)%RR)
-annotate!(uu,0:RR-1, text.(uuuu,18,"Computer Modern"))
-current()
-end
-
+####################################################
+##########################################################################################
 #the first input is the result of a function "bipathpersistence" in BipathmatrixMthod.jl
-function plotintlist(dict_resultof_bipathpersistence,a::Int,b::Int,i_th::Int)#####a=0,b=0
+function plotintlist(dict_resultof_bipathpersistence,i_th::Int)#####a=0,b=0
+    ###check that we have points to plot. 
     X =dict_resultof_bipathpersistence
     dict_int=X[1][i_th]
     if (i_th in keys(X[1])) == false
@@ -287,58 +149,41 @@ function plotintlist(dict_resultof_bipathpersistence,a::Int,b::Int,i_th::Int)###
             return false 
         end
     end
+    ###End "check that we have points to plot" 
+    ###plots points 
     n = X[2]-2
     m = X[3]-2
     RR = n + m +2
-    
+        
     intL = dict_int[1]
-    #intL =  [X[1][1][i] for i in findall(upper_than_diag,X[1][1])]
     intR= dict_int[2]
-    #intR =  [X[2][1][i] for i in findall(upper_than_diag,X[2][1])]
-    infinity = dict_int[4]
-    #println("#[̂0,̂1] is ", length(infinity))
-    
-    #up =  [X[3][i] for i in findall(upper_than_diag,X[3])]
-    #down=  [X[5][i] for i in findall(upper_than_diag,X[5])]
     up =  dict_int[3]
     down= dict_int[5]
     intlist = intervalstocartesiancoordinate(intL,intR,up,down,n,m)
     plot()
-    XX = intervalstocartesiancoordinatewithmulti(intlist,n,m)
-    #a,b = 0,0
-    maxim = maximum(XX[3])
-    col =  cgrad(:lajolla50,rev =true )
+    x_ax, y_ax, multiplicity = intervalstocartesiancoordinatewithmulti(intlist)
+    maxim = maximum(multiplicity)
+    #col = cgrad(:lajolla50,rev =true )
+    col =  cgrad(:turbo )
+
     num = length(col)
     d = div(num,maxim)
 
+## draw labels on a plane
+a=0
+b=0
+vline!([(n+1+a)%RR], linestyle=:dash, color=:black, label="1-hat ="*string(n+1))
+hline!([(n+1+b)%RR], linestyle=:dash, color=:black, label="1-hat ="*string(m+1))
 
-#color area where points are not plotted
-    #plot!([0, n+1], [0, n+1], fillrange=0, fillalpha=0.4, fillcolor=:lightblue,linewidth=0, msw=:0,label=false)
-    #plot!([5,R],[n+1,R],fillrange = [n+1,n+1],fillalpha = 0.4,fillcolor =:lightblue, msw=:0,linewidth=0, label=false)
-    
-for i in 1:length(XX[1])
-    #sizeofpoint= 3 + I[2]/all
-    plot!([(XX[1][i]+a)%RR], [(XX[2][i]+b)%RR], 
-    proj=:identity, ms=6,msw=:0,
-    m=:o#,c =col[min(length(col),XX[3][i]*d)]
-    ,
-    c = col[colorfunc(col,XX[3][i]*d)],
-    lt=:scatter,
-    markerstrokealpha=1, legend = false,
-    xlims=(-1,RR), ylims=(-1,RR),  
-    fa=0.4, xticks=[], yticks=[],
-    grid=true, left_margin = 10Plots.mm, 
-    bottom_margin = 10Plots.mm, 
-    aspect_ratio=:equal, label="")
-end
+vline!([a%RR], linestyle=:dash, color=:black, label="0-hat ="*string(n+1))
 
-vline!([(n+1+a)%RR], linestyle=:dash, color=:red, label="1-hat ="*string(XX[4]+1))
-hline!([(n+1+b)%RR], linestyle=:dash, color=:red, label="1-hat ="*string(XX[5]+1))
-vline!([a%RR], linestyle=:dash, color=:blue, label="0-hat ="*string(XX[4]+1))
-hline!([b%RR], linestyle=:dash, color=:blue, label="0-hat ="*string(XX[5]+1))
-# draw labels
+hline!([b%RR], linestyle=:dash, color=:black, label="0-hat ="*string(m+1))
+vline!([RR], linestyle=:dash, color=:black, label="0-hat ="*string(m+n+1))
 
-hat = L"\widehat{0}"
+#hline!([RR+1], linestyle=:dash, color=:black, label="∞ ="*string(RR+1))
+
+#hat = L"\widehat{0}"
+hat = " "
 lll=[hat]
 for i in 1:n 
     push!(lll,string(i)) 
@@ -350,19 +195,31 @@ for i in 1:m
     push!(lll,string(m-i+1)*"'") 
 end
 uu = -1.5*ones(RR)
-uuuu=shaffle(lll,(RR-a)%RR)
-annotate!(0:RR-1, uu, text.(uuuu,18,"Computer Modern"))
+annotate!(0:RR-1, uu, text.(lll,16,"Computer Modern"))
 uu = -1.5*ones(RR)
-uuuu=shaffle(lll,(RR-b)%RR)
-annotate!(uu,0:RR-1, text.(uuuu,18,"Computer Modern"))
+annotate!(uu,0:RR-1, text.(lll,16,"Computer Modern"))
+#annotate!([-1.5],RR:RR, text.(uuuu,16,"Computer Modern"))
+#plot!(annotation = (RR, -1.5,L"\widehat{0}"))
+
+## End "draw labels on a plane (or torus)"
+
+##plot points
+for i in 1:length(x_ax)
+    plot!([(x_ax[i]+a)%RR], [(y_ax[i]+b)%RR], 
+    proj=:identity, ms=6,msw=:0.3,
+    m=:o,
+    c = col[colorfunc2(col,maxim,multiplicity[i])],#c = col[colorfunc(col,multiplicity[i]*d)],
+    lt=:scatter,
+    markerstrokealpha=1, legend = false,markeralpha=1,
+    xlims=(-1,RR+0.5), ylims=(-1,RR+0.5),  
+    fa=1, xticks=[], yticks=[],
+    grid=true, left_margin = 10Plots.mm, 
+    bottom_margin = 10Plots.mm, 
+    aspect_ratio=:equal, label="")
+end
+## End "plots points "
 current()
 end
 
-dd = Dict()
-dd[1]= "a"
-1 in keys(dd)
-###(6)Exampleintlist
-
 #savefig("bipathPD3.png") 
 #plotintlist(intlist,a,b)
-
