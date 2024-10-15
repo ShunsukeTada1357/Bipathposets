110657a6cca7d8ac91ebe37bbabca1dc4324244d
ShunsukeTada1357 <123849098+ShunsukeTada1357@users.noreply.github.com>
2024-10-15 16:25:02 +0900

    name changed Update Bipathposets.jl
    FSCtoAtypeMethod.jl=>AtypeMethod.jl

--- src/Bipathposets.jl
+++ src/Bipathposets.jl
@@ -2,7 +2,7 @@ module Bipathposets
 include("IsFSC.jl")
 include("FSCvector.jl")
 include("FSC_Contraction_Persistence.jl")
-include("FSCtoAtypeMethod.jl")
+include("AtypeMethod.jl")
 include("BipathMatMethod.jl")
 include("clique.jl")
 include("BipathPD.jl")
