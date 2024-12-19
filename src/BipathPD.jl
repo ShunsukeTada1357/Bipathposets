c8afa5a46ad00280d9167ccb3a11e0ff44334e53
Tadashunsuke <unkoman1357@gmail.com>
2024-12-19 19:19:57 +0900

    BipathPD.jl simplified

--- src/BipathPD.jl
+++ src/BipathPD.jl
@@ -1,8 +1,8 @@
-##This file is for a function plotintlist(intlist,a,b)
-##That visualize bipath intervals.     
+##This file is for the function plotintlist(FSCa,FSCb,i_th::Int) which visualizes intervals in the plane.     
 using Plots
 using LaTeXStrings
-function intervalstocartesiancoordinate(intL,intR,up,down,center,n,m)
+using StatsBase
+function intervalstoplane(intL,intR,up,down,center,n,m)
     intlist =[]
     for I in intL
         a = n+m+3-I[2][2] # an end point of I
@@ -30,94 +30,54 @@ function intervalstocartesiancoordinate(intL,intR,up,down,center,n,m)
     return intlist
 end
 
-function countfunction(intlist)#[I,I,J,J,J]=> [[I,2],[J,3]]
-    lis = copy(intlist)
-    int_num = []
-    while iszero(lis) != true 
-        push!(int_num, [lis[1],length([lis[1] for J in lis if J == lis[1]]) ] ) 
-        lis = [J for J in lis if J != lis[1]]
-    end
-    return int_num
-end
-
-#(5) color of points means the number of intervals C_{n,m}
-function colorfunc(col,max::Int,mult::Int)
-    n = length(col)
-    d = Int64(floor(n/max))
-    if d <= 1
-        return 1
-    elseif max == 1
-        return 1
-    else
-        return min(1+(d)*(mult-1),n)
-    end
+#color of points means the number of intervals in the bipath posets B_{n,m}
+function colorfunc(col, max::Int, mult::Int)
+    d = div(length(col), max)
+    return (d <= 1 || max == 1) ? 1 : min(1 + d * (mult - 1), length(col))
 end
 
 function plotpoints(points,n,m)#points=[[a,b],[c,d],[a,b],...]
     plot()
     RR = n + m +2
-    ints = countfunction(points)
-    maxim = maximum([i[2] for i in ints])
-    col =  cgrad(:turbo )
+    ints = StatsBase.countmap(points) # 点の頻度を辞書に 
+    maxim= maximum(values(ints))
+    col =  cgrad(:turbo)
     num = length(col)
     d = div(num,maxim)
+   
+    # Axis annotation
+    lll=[string(i) for i in 1:n]
+    push!(lll,L"\widehat{1}")
+    append!(lll, [string(m-i+1)*"'" for i in 1:m])
+    uu = -1.5*ones(RR)
+    xaxis = vcat(lll,L"\widehat{0}")
+    yaxis = vcat(L"\widehat{0}",lll)
 
-vline!([(n+1)%RR], linestyle=:dash, color=:black, label="1-hat ="*string(n+1))
-hline!([(n+1)%RR], linestyle=:dash, color=:black, label="1-hat ="*string(m+1))
-hline!([0], linestyle=:dash, color=:black, label="0-hat ="*string(m+1))
-vline!([RR], linestyle=:dash, color=:black, label="0-hat ="*string(m+n+1))
+    annotate!(1:RR, uu, text.(xaxis,10,"Computer Modern"))
+    annotate!(uu,0:RR-1, text.(yaxis,10,"Computer Modern"))
 
-lll=[]
-for i in 1:n 
-    push!(lll,string(i)) 
-end
-push!(lll,L"\widehat{1}")
-for i in 1:m 
-    push!(lll,string(m-i+1)*"'") 
-end
-uu = -1.5*ones(RR)
-xaxe=copy(lll) 
-push!(xaxe,L"\widehat{0}")
-annotate!(1:RR, uu, text.(xaxe,10,"Computer Modern"))
-yaxe= copy(lll)
-insert!(yaxe,1,L"\widehat{0}")
-annotate!(uu,0:RR-1, text.(yaxe,10,"Computer Modern"))
+     # Draw axis lines
+     vline!([(n+1)%RR], linestyle=:dash, color=:black, label="1-hat ="*string(n+1))
+     hline!([(n+1)%RR], linestyle=:dash, color=:black, label="1-hat ="*string(m+1))
+     hline!([0], linestyle=:dash, color=:black, label="0-hat ="*string(m+1))
+     vline!([RR], linestyle=:dash, color=:black, label="0-hat ="*string(m+n+1)) 
 
-for i in 1:length(ints) ##plot points
-    if ints[i][1][1] % RR  !=0 
-       plot!([(ints[i][1][1])%RR], [(ints[i][1][2])%RR], 
-       proj=:identity, ms=6,msw=:0.3,
-       m=:o,
-       c = col[colorfunc(col,maxim,ints[i][2])],#c = col[colorfunc(col,multiplicity[i]*d)],
-       lt=:scatter,
-       markerstrokealpha=1, legend = false,markeralpha=1,left_margin = 10Plots.mm, 
-       bottom_margin = 10Plots.mm, aspect_ratio=:equal, label="",xlims=(-1,RR+0.5), ylims=(-1,RR+0.5),  
-       fa=1, xticks=[], yticks=[])
-    elseif ints[i][1][2] == RR
-        plot!([0], [RR], 
-        proj=:identity, ms=6,msw=:0.3,
-        m=:o,
-        c = col[colorfunc(col,maxim,ints[i][2])],
-        lt=:scatter,
-        markerstrokealpha=1,markeralpha=1,legend = false,left_margin = 10Plots.mm, 
-        bottom_margin = 10Plots.mm, aspect_ratio=:equal,  label="",xlims=(-1,RR+0.5), ylims=(-1,RR+0.5),  
-        fa=1, xticks=[], yticks=[])
-    else
-        plot!([RR], [(ints[i][1][2])], 
-        proj=:identity, ms=6,msw=:0.3,
-        m=:o,
-        c = col[colorfunc(col,maxim,ints[i][2])],
-        lt=:scatter,markeralpha=1, legend = false,left_margin = 10Plots.mm, 
-        bottom_margin = 10Plots.mm  , label="",xlims=(-1,RR+0.5), ylims=(-1,RR+0.5),  
-        fa=1, xticks=[], yticks=[],aspect_ratio=:equal)
-    end
-end ## End "plots points"
+    # Plot points
+    for key in keys(ints) 
+        (p,q) = key[1] % RR  !=0 ? ((key[1])%RR, (key[2])%RR) : key[2] == RR ? (0, RR) : (RR, key[2])
+        plot!([p], [q], 
+        proj=:identity, ms=6,msw=:0.3, m=:o,
+        c = col[colorfunc(col,maxim,ints[key])],#c = col[colorfunc(col,multiplicity[i]*d)],
+        lt=:scatter, markerstrokealpha=1, markeralpha=1, legend = false,
+        left_margin = 10Plots.mm, bottom_margin = 10Plots.mm,
+        label="",xlims=(-1,RR+0.5), ylims=(-1,RR+0.5),
+        fa=1, xticks=[], yticks=[], aspect_ratio=:equal)
+    end     
 current()
 end
-
-#The first input is the result of a function "interval_decomposition" in BipathmatrixMthod.jl
-function plotintlist(dict_resultof_interval_decomposition,i_th::Int) 
-    X =dict_resultof_interval_decomposition
+#This is the desired function.
+function plotintlist(FSCa,FSCb,i_th::Int) 
+    X = interval_decomposition(FSCa,FSCb)
     if (i_th in keys(X[1])) == false #check that we have points to plot. 
         println("no ",i_th," homology.")
         return false
@@ -125,6 +85,6 @@ function plotintlist(dict_resultof_interval_decomposition,i_th::Int)
     dict_int=X[1][i_th]
     n,m = X[2]-2, X[3]-2
     intL, intR, up, center, down = dict_int[1], dict_int[2], dict_int[3], dict_int[4], dict_int[5]
-    intlist = intervalstocartesiancoordinate(intL,intR,up,down,center,n,m)
+    intlist = intervalstoplane(intL,intR,up,down,center,n,m)
     plotpoints(intlist,n,m)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,8 +6,8 @@ using SimpleGraphs
 Test = [[[1],1],[[2],2],[[1,2],3],[[4],4],[[5],5],[[4,5],6],[[2,4],7],[[1,5],8],[[2,5],9],[[2,4,5],10],[[1,2,5],11]]
 Test=[Test,11]
 aa = Bipathposets.interval_decomposition(Test,Test)
-Bipathposets.plotintlist(aa,0)
-Bipathposets.plotintlist(aa,1)
+Bipathposets.plotintlist(Test,Test,0)
+Bipathposets.plotintlist(Test,Test,1)
 ##########################################
 #Example(2) 
 FSCa = [[[1],1],[[2],1],[[3],1],[[4],1],[[5],1],[[1,2],1],[[2,3],1],[[1,3],1],
@@ -18,8 +18,8 @@ FSCb = [[[1],1],[[2],1],[[3],1],[[4],1],[[5],1],[[1,2],1],[[2,3],1],[[1,3],1],
 FSCb =[FSCb,4]
 Bipathposets.baseswithintervals(FSCa)[2]
 aa =Bipathposets.interval_decomposition(FSCa,FSCb)
-Bipathposets.plotintlist(aa,0)
-Bipathposets.plotintlist(aa,1)
+Bipathposets.plotintlist(FSCa,FSCb,0)
+Bipathposets.plotintlist(FSCa,FSCb,1)
 ##########################################
 #Example(3)
 FSCa = [[[1],1],[[2],1],[[3],1],[[4],1],[[5],1],[[6],1],[[1,2],1],[[2,3],1],[[1,3],1],
@@ -30,8 +30,8 @@ FSCb = [[[1],1],[[2],1],[[3],1],[[4],1],[[5],1],[[6],1],[[1,2],1],[[2,3],1],[[1,
 FSCb =[FSCb,4]
 Bipathposets.baseswithintervals(FSCa)[2]
 aa =Bipathposets.interval_decomposition(FSCa,FSCb)
-Bipathposets.plotintlist(aa,0)
-Bipathposets.plotintlist(aa,1)
+Bipathposets.plotintlist(FSCa,FSCb,0)
+Bipathposets.plotintlist(FSCa,FSCb,1)
 using Plots
 savefig("bipathPD1th_empty.png") 
 ##########################################
@@ -39,14 +39,14 @@ savefig("bipathPD1th_empty.png")
 FSCa = [[ [[1],1], [[2],1], [[1,2],2] ],  5]
 FSCb = [[ [[1],1], [[2],1], [[1,2],3] ],  5]
 aa =Bipathposets.interval_decomposition(FSCa,FSCb)
-Bipathposets.plotintlist(aa,0)
+Bipathposets.plotintlist(FSCa,FSCb,0)
 ##########################################
 ##########################################
 #Example (5) 
 FSCa =[ [ [[1],1],[[2],1],[[3],1],[[4],1],[[5],1], [[1,2],1], [[1,3],1], [[2,3],1], [[3,4],1], [[3,5],1], [[4,5],1], [[1,5],2], [[1,4],3], [[3,4,5],4],[[1,3,5],5]   ] ,5]
 FSCb =[[ [[1],1],[[2],1],[[3],1],[[4],1], [[5],1], [[1,2],1], [[1,3],1], [[2,3],1], [[3,4],1], [[3,5],1], [[4,5],1], [[1,4],2], [[1,5],3], [[1,3,5],3], [[3,4,5],4] ],  4]
 aa =Bipathposets.interval_decomposition(FSCa,FSCb)
-Bipathposets.plotintlist(aa,0)
+Bipathposets.plotintlist(FSCa,FSCb,0)
 ##########################################
 #Example(6) random model
 path1, path2 = Bipathposets.get_rectangular_paths([0.4,0.4],[0.7,0.7],5)
@@ -62,4 +62,4 @@ Bipathposets.plotintlist(aa,1)
 FSCa =[ [ [[1],1],[[2],1],[[3],1],[[4],1],[[5],1], [[1,2],1], [[1,3],1], [[2,3],1], [[3,4],1], [[3,5],1], [[4,5],1], [[1,5],2], [[1,4],3], [[3,4,5],4],[[1,3,5],5]   ] ,5]
 FSCb =[[ [[1],1],[[2],1],[[3],1],[[4],1], [[5],1], [[1,2],1], [[1,3],1], [[2,3],1], [[3,4],1], [[3,5],1], [[4,5],1], [[1,4],2], [[1,5],3], [[1,3,5],3], [[3,4,5],4] ],  4] 
 aa =Bipathposets.interval_decomposition(FSCa,FSCb)
-Bipathposets.plotintlist(aa,0)
+Bipathposets.plotintlist(FSCa,FSCb,0)
