8c91d5db96c96c2384a9cd3ab6255074ca6e3aab
Tadashunsuke <unkoman1357@gmail.com>
2025-04-11 12:10:41 +0900

    modified

--- test/Bipath_Image.jl
+++ test/Bipath_Image.jl
@@ -4,6 +4,9 @@
 #(2) to change image data into 0-1 matrices.
 #Finally, we compute bipath persistence diagrams of bipath filtrations of image data.
 ##################################################
+using Images
+using ImageView
+using Bipathposets
 #(1) how to read image files automatically from a folder.
 using Glob 
 # Read all PNG files from two folders.
@@ -29,8 +32,6 @@ display(Gray.(mat))
 """
 
 #Finally, we compute bipath PD. 
-using Images
-using ImageView
 # 画像を読み込こむ → gray scale -> 0-1 matrix (配列に格納)
 #imgs = [load(path) for path in img_paths]
 imgsUp = [image_to_binary_matrix(Gray.(load(path))) for path in img_paths_up]
