c059d4be51948cbbc78b575ed1175f1cf00a9116
ShunsukeTada1357 <kyorochan1357@gmail.com>
2026-04-13 17:06:21 +0900

    examples

--- src/clique.jl
+++ src/clique.jl
@@ -2,95 +2,94 @@
 import Combinatorics as Comb
 import SimpleGraphs as SG
 
-#Get a bipath from R^2.
-function get_rectangular_paths(init,ending,partition::Int) #init, ending ∈R^2
-    underpath =[]
-    uppath=[]
-    lx = (ending[1] - init[1])/partition
-    ly = (ending[2] - init[2])/partition
+#Get a bipath from R^2.# clique.jl
+# Contributed by Toshitaka Aoki
+import Combinatorics as Comb
+import SimpleGraphs as SG
+
+# Get a bipath from R^2.
+function get_rectangular_paths(init, ending, partition::Int) # init, ending ∈ R^2
+    underpath = []
+    uppath = []
+
+    lx = (ending[1] - init[1]) / partition
+    ly = (ending[2] - init[2]) / partition
+
     for i in 1:partition
-        push!(underpath,init+[lx*(i-1),0])   
-        push!(uppath,init+[0,ly*(i-1)])  
+        push!(underpath, init + [lx * (i - 1), 0])
+        push!(uppath,   init + [0, ly * (i - 1)])
     end
-    push!(underpath,[ending[1],init[2]])
-    push!(uppath,[init[1],ending[2]])
+
+    push!(underpath, [ending[1], init[2]])
+    push!(uppath,    [init[1], ending[2]])
+
     for i in 1:partition-1
-        push!(underpath,[ending[1],init[2]+ly*(i)])
-        push!(uppath,[init[1]+lx*(i),ending[2]])
-    end  
-    push!(underpath,ending)
-    push!(uppath,ending)
+        push!(underpath, [ending[1], init[2] + ly * i])
+        push!(uppath,    [init[1] + lx * i, ending[2]])
+    end
+
+    push!(underpath, ending)
+    push!(uppath, ending)
+
     return uppath, underpath
 end
 
- 
+function _clique_random_core(G, faces, path1, path2)
+    edges = collect(G.E)
+    nvtx = length(G.V)
+    nedges = binomial(nvtx, 2)
 
-function clique_random(G,faces,path1,path2)##Contributed by Toshitaka Aoki
-    SE1 = collect(G.E)  # G のエッジ（辺）のリスト
-    n = length(G.V)     # 頂点数
-    m = binomial(n,2)   # 完全グラフにおける最大エッジ数
-    l = 2^n - 1         # 全てのfaceの数
-    w1 = rand(m);
-    sort!(w1)           #ランダムな m 個の数値を持つリストを作成し、ソート。SE1の重みに使う。
-    w2 = Dict(zip(SE1,rand(m)));  #各エッジにランダムな重みを割り当てる辞書を作成。
-    SE2 = sort(SE1, by = x -> w2[x]) #w2の値でエッジSE1をソート
-    d1 = Vector(undef, l);
-    d2 = Vector(undef, l);
-    
-    #初期状態（頂点単体のクリーク）
-    for i in 1:n 
-        d1[i] = [[i], 1] 
-        d2[i] = [[i], 1] 
+    # vertices + higher-dimensional faces
+    total_len = nvtx + length(faces)
+
+    w1 = rand(nedges)
+    sort!(w1)
+
+    w2 = Dict(zip(edges, rand(nedges)))
+    edges_sorted_by_w2 = sort(edges, by = e -> w2[e])
+
+    d1 = Vector{Any}(undef, total_len)
+    d2 = Vector{Any}(undef, total_len)
+
+    # vertices
+    for i in 1:nvtx
+        d1[i] = [[i], 1]
+        d2[i] = [[i], 1]
     end
- 
-    for i in 1:length(faces)
-        f = faces[i]
-        birth_f = [w1[findlast(e -> collect(e) ⊆ f, SE1)], w2[SE2[findlast(e -> collect(e) ⊆ f, SE2)] ]]#∈R^2
-        d1[i+n] = [f, findfirst(p -> (birth_f[1] <= p[1]) && (birth_f[2] <= p[2]), path1)]
-        d2[i+n] = [f, findfirst(p -> (birth_f[1] <= p[1]) && (birth_f[2] <= p[2]), path2)] 
-        
-        if i % 10000 == 0 
+
+    for (i, f) in enumerate(faces)
+        idx1 = findlast(e -> collect(e) ⊆ f, edges)
+        idx2 = findlast(e -> collect(e) ⊆ f, edges_sorted_by_w2)
+
+        birth_f = [
+            w1[idx1],
+            w2[edges_sorted_by_w2[idx2]]
+        ]
+
+        d1[nvtx + i] = [f, findfirst(p -> (birth_f[1] <= p[1]) && (birth_f[2] <= p[2]), path1)]
+        d2[nvtx + i] = [f, findfirst(p -> (birth_f[1] <= p[1]) && (birth_f[2] <= p[2]), path2)]
+
+        if i % 10000 == 0
             println(i)
         end
     end
 
-    d1 = d1[findall(x -> d1[x][2] != nothing, 1:l)]
-    d2 = d2[findall(x -> d2[x][2] != nothing, 1:l)]
-    return [d1,length(path1)], [d2,length(path2)] 
-end  
+    valid_idx1 = findall(x -> x[2] !== nothing, d1)
+    valid_idx2 = findall(x -> x[2] !== nothing, d2)
 
-function clique_random_ith(n,path1,path2,ith)
-    G = SG.Complete(n) 
-    faces = collect(Combinatorics.powerset(collect(G.V), 2, ith+2))
-    l = length(faces)  
-    SE1 = collect(G.E)  # G のエッジ（辺）のリスト
-    n = length(G.V)     # 頂点数
-    m = binomial(n,2)   # 完全グラフにおける最大エッジ数       
-    w1 = rand(m);
-    sort!(w1)           #ランダムな m 個の数値を持つリストを作成し、ソート。SE1の重みに使う。
-    w2 = Dict(zip(SE1,rand(m)));  #各エッジにランダムな重みを割り当てる辞書を作成。
-    SE2 = sort(SE1, by = x -> w2[x]) #w2の値でエッジSE1をソート
-    d1 = Vector(undef, l+n);
-    d2 = Vector(undef, l+n);
-    
-    #初期状態（頂点単体のクリーク）
-    for i in 1:n 
-        d1[i] = [[i], 1] 
-        d2[i] = [[i], 1] 
-    end
- 
-    for i in 1:length(faces)
-        f = faces[i]
-        birth_f = [w1[findlast(e -> collect(e) ⊆ f, SE1)], w2[SE2[findlast(e -> collect(e) ⊆ f, SE2)] ]]#∈R^2
-        d1[i+n] = [f, findfirst(p -> (birth_f[1] <= p[1]) && (birth_f[2] <= p[2]), path1)]
-        d2[i+n] = [f, findfirst(p -> (birth_f[1] <= p[1]) && (birth_f[2] <= p[2]), path2)] 
-        
-        if i % 10000 == 0 
-            println(i)
-        end
-    end
+    d1 = d1[valid_idx1]
+    d2 = d2[valid_idx2]
 
-    d1 = d1[findall(x -> d1[x][2] != nothing, 1:l)]
-    d2 = d2[findall(x -> d2[x][2] != nothing, 1:l)]
-    return [d1,length(path1)], [d2,length(path2)] 
-end  
+    return [d1, length(path1)], [d2, length(path2)]
+end
+
+function clique_random(G, faces, path1, path2)
+    return _clique_random_core(G, faces, path1, path2)
+end
+
+function clique_random_ith(n, path1, path2, ith)
+    G = SG.Complete(n)
+    faces = collect(Comb.powerset(collect(G.V), 2, ith + 2))
+    return _clique_random_core(G, faces, path1, path2)
+end
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,44 @@
 using Bipathposets
 using Combinatorics
-using SimpleGraphs 
+using BenchmarkTools
+using AbstractAlgebra
+R = GF(2)
+
+function random_GF2_matrix(n::Int, m::Int, p::Float64=0.5)
+    return matrix(R, n, m, [R(rand() < p ? 1 : 0) for _ in 1:(n*m)])
+end
+A = random_GF2_matrix(20, 20, 0.5)
+
+function dense_to_sparse_columns(BB)
+    nrows = size(BB, 1)
+    ncols = size(BB, 2)
+
+    cols = Vector{Vector{Int}}(undef, ncols)
+
+    for j in 1:ncols
+        nz = Int[]
+        for i in 1:nrows
+            if !iszero(BB[i, j])
+                push!(nz, i)
+            end
+        end
+        cols[j] = nz
+    end
+
+    return cols
+end
+
 ##########################################
 #Example(1) 
 Test = [[[1],1],[[2],2],[[1,2],3],[[4],4],[[5],5],[[4,5],6],[[2,4],7],[[1,5],8],[[2,5],9],[[2,4,5],10],[[1,2,5],11]]
 Test=[Test,11]
-aa = Bipathposets.interval_decomposition(Test,Test)
-Bipathposets.plotintlist(aa,0)
-Bipathposets.plotintlist(aa,1)
+
+aa = Bipathposets.baseswithintervals(Test)
+aa =Bipathposets.interval_decomposition(Test, Test, verbose=true)
+
+Bipathposets.plot_bipath_diagram(aa,0)
+Bipathposets.plot_bipath_diagram(aa,1)
+
 ##########################################
 #Example(2) 
 FSCa = [[[1],1],[[2],1],[[3],1],[[4],1],[[5],1],[[1,2],1],[[2,3],1],[[1,3],1],
@@ -16,11 +47,89 @@ FSCa=[FSCa,5]
 FSCb = [[[1],1],[[2],1],[[3],1],[[4],1],[[5],1],[[1,2],1],[[2,3],1],[[1,3],1],
 [[1,5],2],[[3,5],2],[[4,5],3],[[1,4],3],[[1,3,5],3],[[3,4],4]]
 FSCb =[FSCb,4]
-Bipathposets.baseswithintervals(FSCa)[2]
+Bipathposets.baseswithintervals(FSCa)[1]
 aa =Bipathposets.interval_decomposition(FSCa,FSCb)
-Bipathposets.plotintlist(aa,0)
-Bipathposets.plotintlist(aa,1)
-##########################################
+Bipathposets.plot_bipath_diagram(aa,0)
+Bipathposets.plot_bipath_diagram(aa,1)
+###################################
+
+A1 = [
+    0 0 0 0 0 0 0 0 0 0;
+    0 1 1 1 0 0 0 0 0 0;
+    0 1 0 0 0 0 0 0 0 0;
+    0 1 0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 1 1 1;
+    0 0 0 0 0 0 0 1 0 1;
+    1 1 1 0 0 0 0 1 1 1;
+    1 0 1 0 0 0 0 0 0 0;
+    1 0 1 0 0 0 0 0 0 0;
+    1 1 1 0 0 0 0 0 0 0
+]
+
+A2 = [
+    0 0 0 0 0 0 0 1 1 1;
+    0 0 0 0 0 0 0 1 0 1;
+    0 0 0 1 0 0 0 1 1 1;
+    0 0 0 1 0 0 0 0 0 0;
+    0 0 1 1 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 0 0;
+    0 0 0 1 0 0 0 0 0 0;
+    0 0 1 0 1 0 0 0 0 0;
+    0 0 0 1 0 0 0 0 0 0
+]
+
+A3 = [
+    0 0 0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 1 0;
+    0 0 0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 0 0;
+    0 0 0 0 0 1 1 1 0 0;
+    0 0 0 0 0 1 0 1 0 0;
+    0 0 0 0 0 1 1 1 0 0;
+    0 0 0 1 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 0 0
+]
+
+A4 = [
+    0 0 0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 1 0 0 0;
+    0 0 0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 0 0
+]
+
+A5 = [
+    0 0 0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 0 0;
+    0 0 1 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 1 0 1 0;
+    0 0 0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 0 0
+]
+
+A_list = [A1, A2, A3, A4, A5]
+B_list = Bipathposets.make_or_filtration(A_list)
+FSCa = Bipathposets.cubical_filtration_to_FSC(B_list)
+
+aa =Bipathposets.interval_decomposition_cubical(FSCa,FSCa)
+Bipathposets.plot_bipath_diagram(aa,0)
+Bipathposets.plot_bipath_diagram(aa,1)
+
+cls=Bipathposets.bipath_records_cubical(FSCa, FSCa)
+Bipathposets.interactive_bipath_viewer_all(cls, B_list, B_list, FSCa, FSCa; dim=1)
+Bipathposets.interactive_bipath_viewer(cls, B_list, B_list, FSCa, FSCa; dim=1)
+###################################
 #Example(3)
 FSCa = [[[1],1],[[2],1],[[3],1],[[4],1],[[5],1],[[6],1],[[1,2],1],[[2,3],1],[[1,3],1],
 [[1,5],2],[[1,4],3],[[4,5],3],[[3,4],3],[[5,6],3],[[3,5],4],[[1,3,5],5]]
@@ -30,8 +139,8 @@ FSCb = [[[1],1],[[2],1],[[3],1],[[4],1],[[5],1],[[6],1],[[1,2],1],[[2,3],1],[[1,
 FSCb =[FSCb,4]
 Bipathposets.baseswithintervals(FSCa)[2]
 aa =Bipathposets.interval_decomposition(FSCa,FSCb)
-Bipathposets.plotintlist(aa,0)
-Bipathposets.plotintlist(aa,1)
+Bipathposets.plot_bipath_diagram(aa,0)
+Bipathposets.plot_bipath_diagram(aa,1)
 using Plots
 savefig("bipathPD1th_empty.png") 
 ##########################################
@@ -39,40 +148,35 @@ savefig("bipathPD1th_empty.png")
 FSCa = [[ [[1],1], [[2],1], [[1,2],2] ],  5]
 FSCb = [[ [[1],1], [[2],1], [[1,2],3] ],  5]
 aa =Bipathposets.interval_decomposition(FSCa,FSCb)
-Bipathposets.plotintlist(aa,0)
+Bipathposets.plot_bipath_diagram(aa,0)
+
 ##########################################
 ##########################################
 #Example (5) 
 FSCa =[ [ [[1],1],[[2],1],[[3],1],[[4],1],[[5],1], [[1,2],1], [[1,3],1], [[2,3],1], [[3,4],1], [[3,5],1], [[4,5],1], [[1,5],2], [[1,4],3], [[3,4,5],4],[[1,3,5],5]   ] ,5]
 FSCb =[[ [[1],1],[[2],1],[[3],1],[[4],1], [[5],1], [[1,2],1], [[1,3],1], [[2,3],1], [[3,4],1], [[3,5],1], [[4,5],1], [[1,4],2], [[1,5],3], [[1,3,5],3], [[3,4,5],4] ],  4]
 aa =Bipathposets.interval_decomposition(FSCa,FSCb)
-Bipathposets.plotintlist(aa,0)
+Bipathposets.plot_bipath_diagram(aa,1)
 ##########################################
-#Example(6) random model
-path1, path2 = Bipathposets.get_rectangular_paths([0.4,0.4],[0.7,0.7],5)
-n =15
-G = Complete(n) 
-faces = collect(Combinatorics.powerset(collect(G.V), 2, n))##修正する
-faces[1000]
-aa=Bipathposets.clique_random(G,faces,path1,path2)
-FSCa = aa[1]
-FSCb = aa[2]
 
-aa =Bipathposets.interval_decomposition(FSCa,FSCb)
-aa[1]
-Bipathposets.plotintlist(aa,1)
-#Example(8) random model ith-homology
-path1, path2 = Bipathposets.get_rectangular_paths([0.3,0.3],[0.6,0.6],5)
-aa=Bipathposets.clique_random_ith(30,path1,path2,1)
-FSCa = aa[1]
-FSCb = aa[2]
-
-aa =Bipathposets.interval_decomposition(FSCa,FSCb)
-aa[1]
-Bipathposets.plotintlist(aa,1)
-
-#Example(7)
+#Example(6)
 FSCa =[ [ [[1],1],[[2],1],[[3],1],[[4],1],[[5],1], [[1,2],1], [[1,3],1], [[2,3],1], [[3,4],1], [[3,5],1], [[4,5],1], [[1,5],2], [[1,4],3], [[3,4,5],4],[[1,3,5],5]   ] ,5]
 FSCb =[[ [[1],1],[[2],1],[[3],1],[[4],1], [[5],1], [[1,2],1], [[1,3],1], [[2,3],1], [[3,4],1], [[3,5],1], [[4,5],1], [[1,4],2], [[1,5],3], [[1,3,5],3], [[3,4,5],4] ],  4] 
 aa =Bipathposets.interval_decomposition(FSCa,FSCb)
-Bipathposets.plotintlist(aa,1)
+Bipathposets.plot_bipath_diagram(aa,1)
+aa = Bipathposets.baseswithintervals(FSCa)
+
+#Example(8)
+FSCa = [[[1],1],[[2],1],[[3],1],[[4],1],[[5],1],[[1,2],1],[[2,3],1],[[1,3],2],
+[[1,4],2],[[1,5],2],[[4,5],2],[[3,4],3],[[3,5],4],[[1,3,5],5]]
+FSCa=[FSCa,5]
+FSCb = [[[1],1],[[2],1],[[3],1],[[4],1],[[5],1],[[1,2],1],[[2,3],1],[[1,3],2],
+[[1,5],2],[[3,5],2],[[4,5],3],[[1,4],3],[[3,4],4], [[1,3,5],4]]
+FSCb =[FSCb,4]
+Bipathposets.baseswithintervals(FSCa)[2]
+aa =Bipathposets.interval_decomposition(FSCa,FSCb)
+Bipathposets.plot_bipath_diagram(aa,0)
+Bipathposets.plot_bipath_diagram(aa,1)
+
+using Plots
+savefig("aaa.png")
