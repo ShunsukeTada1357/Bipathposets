da404f2422766032ec8217de1ee8062eac828b20
ShunsukeTada1357 <kyorochan1357@gmail.com>
2026-04-13 16:59:44 +0900

    changed to sparse matrix for fast computation

--- src/AtypeMethod.jl
+++ src/AtypeMethod.jl
@@ -1,86 +1,117 @@
+#AtypeMethod.jl
 #Using this code, we obtain intervals and i-th basis from FSC(filtered).
 #This code generates a matrix encoding boundary operator of simplices. 
-using AbstractAlgebra
-R = GF(2);
-function getB(FSC)
-    n = length(FSC[1])  
-    B = zero_matrix(R,n,n)
-    inj = Dict(zip([s[1] for s in FSC[1]], Vector(1:n) )) #injection: simplex -> index
-    for i in 1:n
-        if length(FSC[1][i][1]) != 1 # boundary_operation(FS[1][i][1])!=0
-            S = boundary_operation(FSC[1][i][1])
-            for s in S
-                B[inj[s],i] = 1
-            end
-        end
-    end
-    return B
+using OrderedCollections
+
+
+# cols_sparse is a sparse reduced boundary representation (vector of support indices)
+function interval_lt(I::Vector{Int}, J::Vector{Int})
+    return I[1] < J[1] || (I[1] == J[1] && I[2] < J[2])
 end
 
-function getBhat_and_basechange(BB) #BB is a boundary matrix from a filtered simplicial complex over a field with two elements.
-    Bhat = copy(BB)
-    col_change=[]
-    for j in 2 : size(BB)[2]
-        i =1
-        while i<j
-            if  (find_lowest_nonzero(Bhat[:,i]) == find_lowest_nonzero(Bhat[:,j])) && find_lowest_nonzero(Bhat[:,j]) != -1
-                Bhat[:,j] = Bhat[:,j]+Bhat[:,i]
-                push!(col_change,[i,j])
-                i= 1
-            else
-                i =i+1
-            end 
-        end
-    end  
-    return [Bhat,col_change]
-end
-
-function get_intervals_with_basis(FSC, Bhat, info_col_change)
+function get_intervals_with_representatives(FSC, cols_sparse, info_col_change)
     info = info_col_change
-    d = Dict()
-        
-    basis =  [[s[1]] for s in FSC[1]]    #equal to FS[1]
-    imagebasis = []
-    E = Vector(1: length(FSC[1]))
-    for i in 1 : length(FSC[1])
-        j = find_lowest_nonzero(Bhat[:,i])
-        if j >= 0
-            basisI = [basis[k][1] for k in 1:length(FSC[1]) if Bhat[k,i] !=0 ]
-            interval = [j,i-1]
-            d[basisI] = interval
-            push!(imagebasis,[basisI,interval])
-            setdiff!(E,[i,j])
-        end 
-    end
+    n = length(FSC[1])
 
-    newbasis = [[s[1]] for s in FSC[1] ] #equal to FSC[1]
-    for vec in info
-        for i in 1 : length(newbasis[vec[1]])
-             push!(newbasis[vec[2]], newbasis[vec[1]][i])
+    basis = [[s[1]] for s in FSC[1]]
+    newbasis = [[s[1]] for s in FSC[1]]
+
+    # finite intervals
+    imagebasis = Vector{Tuple{Any,Vector{Int}}}()
+
+    # 全 intervals を (basis, interval) で持つ
+    pair_list = Vector{Tuple{Any,Vector{Int}}}()
+
+    # old E = collect(1:n) / setdiff! の代わり
+    alive = trues(n)
+
+    # finite intervals
+    for i in 1:n
+        j = low_of_sparse_column(cols_sparse[i])
+
+        if j >= 0
+            supp = cols_sparse[i]
+            basisI = [basis[k][1] for k in supp]
+            interval = [j, i - 1]
+
+            push!(pair_list, (basisI, interval))
+            push!(imagebasis, (basisI, interval))
+
+            alive[i] = false
+            alive[j] = false
         end
     end
 
-    for i in E
-        d[newbasis[i]] =[i,length(FSC[1])]
+    # 列変形情報を representative に反映
+    for (src, dst) in info
+        append!(newbasis[dst], newbasis[src])
     end
-    pairs = sort(d;byvalue=true)
-    return  [pairs,imagebasis] #It returns pairs of intervals and bases. 
+
+    # infinite intervals
+    for i in 1:n
+        if alive[i]
+            push!(pair_list, (newbasis[i], [i, n]))
+        end
+    end
+
+    # 区間で sort
+    sort!(pair_list; by = last, lt = interval_lt)
+
+    pairs = OrderedCollections.OrderedDict{Any,Any}()
+    for (b, intv) in pair_list
+        pairs[b] = intv
+    end
+
+    # imagebasis は旧版に合わせて [basis, interval] 形式
+    imagebasis_oldstyle = [[b, intv] for (b, intv) in imagebasis]
+
+    return [pairs, imagebasis_oldstyle]
 end
+
+##############################################################
+#We contract birth and death of topological features [b,d] to some [b',d']. 
+function contractbirth(birth,FSC)# I is an interval [b,d]
+    return FSC[1][birth][2]
+end
+function contractdeath(death,FSC)
+    if death == length(FSC[1])
+        return FSC[2]
+    else
+        return FSC[1][death+1][2]-1
+    end
+end
+function contractinterval(I, FSC)
+    return [contractbirth(I[1],FSC), contractdeath(I[2],FSC)]
+end
+#################################################################
 
 function baseswithintervals(FSC)
-    fsc = [[FSC[1][i][1],i] for i in 1: length(FSC[1])]
-    Bhat, info_col = getBhat_and_basechange( getB([fsc,"any"]))
-    pairs,imagebasis = get_intervals_with_basis(FSC,Bhat,info_col)
+    fsc = [[FSC[1][i][1], i] for i in 1:length(FSC[1])]
+
+    cols0 = get_boundary_columns_sparse([fsc, "any"])
+    cols_red, info_col = reduction_and_basechange_sparse_from_columns(cols0)
+
+    pairs, imagebasis = get_intervals_with_representatives(FSC, cols_red, info_col)
 
     newpairs = Dict()
     for k in keys(pairs)
-        Interval = contractinterval(pairs[k],FSC)
-        if Interval[1]<= Interval[2]
+        Interval = contractinterval(pairs[k], FSC)
+        if Interval[1] <= Interval[2]
             newpairs[k] = Interval
         end
-    end   
-    newpairs = sort(newpairs;byvalue=true)
-    imagebasisleft = [a[1] for a in imagebasis if contractinterval(a[2],FSC)[2]==0] ## The left image of the vector space.  
-    imagebasis = [a[1] for a in imagebasis]                                         ## We do not need intervals now. 
-    return [newpairs,imagebasisleft, imagebasis]                                       ## It returns pairs (intervals and bases) and basis of image of boundary operation.        
+    end
+
+pairvec = collect(newpairs)
+sort!(pairvec; by = last, lt = interval_lt)
+
+newpairs_sorted = OrderedCollections.OrderedDict{Any,Any}()
+for p in pairvec
+    newpairs_sorted[first(p)] = last(p)
+end
+
+newpairs = newpairs_sorted
+    imagebasisleft = [a[1] for a in imagebasis if contractinterval(a[2], FSC)[2] == 0]
+    imagebasis_only = [a[1] for a in imagebasis]
+
+    return [newpairs, imagebasisleft, imagebasis_only]
 end
